0344723132e4e7f2d57b79d42f97da5a592f966c
Ashu Gupta <ashugpt1812@gmail.com>
2024-04-13 02:13:00 +0530

    feat: footer with gh repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "../components/layout/navbar";
+import Footer from "../components/layout/footer";
 import { WalletProvider } from "@/providers/wallet-provider";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,6 +24,7 @@ export default function RootLayout({
           <main className="flex flex-col w-screen h-screen items-center px-24 py-20">
             <Navbar />
             {children}
+            <Footer />
           </main>
         </WalletProvider>
       </body>
